c08c59499f45f2dde9374f5e31fa4403b5fa3cf4
Abhishek Sharma <65117467+ioabhishek@users.noreply.github.com>
2022-03-31 11:48:44 +0530

    Added solution - LeetHub

---
+++ Reverse a String - GFG/reverse-a-string.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+def reverseWord(s):
+    #your code here
+    rev = ""
+    for i in range(len(s)-1, -1, -1):
+        rev += s[i]
+    return rev
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        print(reverseWord(s))
+        t = t-1
+
+# } Driver Code Ends
