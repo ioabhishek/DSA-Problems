f561df21bae3997dd36d90804a6c7ca260ee57d0
Abhishek Sharma <65117467+ioabhishek@users.noreply.github.com>
2022-04-11 09:05:39 +0530

    Time: 479 ms (84.64%), Space: 25.9 MB (73.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,19 +1,26 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         d = {}
-        flag = True
         for i in range(len(nums)):
             if nums[i] in d:
-                d[nums[i]] += 1
-            else:
-                d[nums[i]] = 1
-                
-        for x,y in d.items():
-            if y >= 2:
-                flag = True
+                return True
                 break
             else:
-                flag = False
+                d[nums[i]] = 1
         
-        return flag
+#         d = {}
+#         flag = True
+#         for i in range(len(nums)):
+#             if nums[i] in d:
+#                 d[nums[i]] += 1
+#             else:
+#                 d[nums[i]] = 1
+                
+#         for x,y in d.items():
+#             if y >= 2:
+#                 flag = True
+#                 break
+#             else:
+#                 flag = False
         
+#         return flag
